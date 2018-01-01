4810928be94b72f5383ab2afdad5dbfcc36fe37a
Daniel Torres <jesusdanieltc@gmail.com>
2017-12-31 17:09:05 -0700

    Add conditions in project

--- twitter.py
+++ twitter.py
@@ -7,7 +7,16 @@ infotweet = {}
 print("Bienvenidos a la aplicacion")
 usuario = input("Cual es tu usuario? ")
 tweet = input("¿Que esta pasando? ")
-likes = input("Cuantos likes? ")
-retweets = input("Cuantos retweets? ")
+likes = int(input("Cuantos likes? "))
+retweets = int(input("Cuantos retweets? "))
 infotweet = {"Autor": usuario, "Mensaje": tweet, "Likes": likes, "Retweets": retweets}
 print(str(infotweet))
+if likes > 2:
+	print("Su tweet tiene 'me gustas'")
+elif likes == 1:
+   	print("Su tweet solo tiene un me gusta")
+elif likes == 2:
+    print("Su tweet solo tienes dos me gustas")
+else:
+	print("Su tweet no tiene me gustas")
+if(retweets > 0): print("Su tweet tiene retweets")
